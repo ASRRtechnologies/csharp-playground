3579892da025825a69eafcd2269568fcb21fd03d
Amar Ramdas <amar.ramdas@asrr.nl>
2022-04-25 19:09:38 +0200

    Commit all files

---
+++ csharp-playground/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using csharp_playground;
+
+Console.WriteLine("Hello, World!");
+
+JsonSamples jsonSamples = new JsonSamples();
+
+jsonSamples.CreateJsonUsingJObject();
+jsonSamples.CreateJsonUsingOOPObject();
+jsonSamples.CreateJsonUsingDynamicAccessor();
+
